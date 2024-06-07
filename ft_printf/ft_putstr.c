7c6e565d2aed6b397de1f778ae332a4028d52492
sufin99 <sufianzaghdad@gmail.com>
2024-06-07 13:34:15 +0200

    Correción final ft_putstr

--- ft_printf/ft_putstr.c
+++ ft_printf/ft_putstr.c
@@ -6,27 +6,23 @@
 /*   By: szaghdad <szaghdad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 12:44:53 by szaghdad          #+#    #+#             */
-/*   Updated: 2024/06/07 13:01:29 by szaghdad         ###   ########.fr       */
+/*   Updated: 2024/06/07 13:30:42 by szaghdad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_strlen(char *str)
+int	ft_putstr(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (ft_putstr("(null)"));
 	while (str[i] != '\0')
 	{
+		ft_putchar(str[i]);
 		i++;
 	}
 	return (i);
-}
-
-int	ft_putstr(char *s)
-{
-	if (!s)
-		return (write(1, "(null)", 6));
-	return (write(1, s, ft_strlen(s)));
 }
